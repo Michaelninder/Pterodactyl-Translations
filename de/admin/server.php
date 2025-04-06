7e1c08b37b4c70294476f9bc60c47897fac93866
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-04-06 07:28:39 +0200

    Update server.php

--- de/admin/server.php
+++ de/admin/server.php
@@ -4,13 +4,13 @@ return [
     'exceptions' => [
         'no_new_default_allocation' => 'You are attempting to delete the default allocation for this server but there is no fallback allocation to use.',
         'marked_as_failed' => 'This server was marked as having failed a previous installation. Current status cannot be toggled in this state.',
-        'bad_variable' => 'There was a validation error with the :name variable.',
+        'bad_variable' => 'Es gab einen Fehler beim überprüven der :name Variable.',
         'daemon_exception' => 'There was an exception while attempting to communicate with the daemon resulting in a HTTP/:code response code. This exception has been logged. (request id: :request_id)',
-        'default_allocation_not_found' => 'The requested default allocation was not found in this server\'s allocations.',
+        'default_allocation_not_found' => 'Der angefragte standart Standort wurde nicht in den Addressem des Servers gefunden.',
     ],
     'alerts' => [
         'startup_changed' => 'The startup configuration for this server has been updated. If this server\'s nest or egg was changed a reinstall will be occurring now.',
-        'server_deleted' => 'Server has successfully been deleted from the system.',
+        'server_deleted' => 'Der Server wurde Erfolgreich von diesem Systhhem gelöscht.',
         'server_created' => 'Server was successfully created on the panel. Please allow the daemon a few minutes to completely install this server.',
         'build_updated' => 'The build details for this server have been updated. Some changes may require a restart to take effect.',
         'suspension_toggled' => 'Server suspension status has been changed to :status.',
