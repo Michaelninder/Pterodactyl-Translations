2e0230f1f96a3223d24c622e4a38f9e0ad701067
Michaelninder <151365993+Michaelninder@users.noreply.github.com>
2025-04-07 07:46:50 +0200

    Update de/dashboard/index.php

--- de/dashboard/index.php
+++ de/dashboard/index.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'search' => 'Search for servers...',
+    'search' => 'Suchen nach Servern...',
     'no_matches' => 'There were no servers found matching the search criteria provided.',
     'cpu_title' => 'CPU',
-    'memory_title' => 'Memory',
+    'memory_title' => 'Arbeitsspeicher',
 ];
